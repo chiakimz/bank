e327afdb1d61c5ce30e395cc8b63a718544d8b13
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-04-23 18:15:08 +0100

    added print function to show all transaction history, and also changed push to upshift for order

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -8,15 +8,22 @@ class Transaction
 
   def add_deposit(n)
     @balance.push(n)
-    record = Account.new.date + '||' + n.to_s + '||' + ' ||' + @balance.inject(0, :+).to_s
-    @history.push(record)
+    record = Account.new.date + ' || ' + "%.2f" % n + ' || ' + '  || ' + "%.2f" % @balance.inject(0, :+)
+    @history.unshift(record)
     record
   end
 
   def withdraw(n)
     @balance.push(-n)
-    record = Account.new.date + '||' + ' ||' + n.to_s + '||' + @balance.inject(0, :+).to_s
-    @history.push(record)
+    record = Account.new.date + ' || ' + '  || ' + "%.2f" % n + ' || ' + "%.2f" % @balance.inject(0, :+)
+    @history.unshift(record)
     record
+  end
+
+  def print
+    @history.unshift(Account.new.default)
+    for each_transaction in @history
+      puts each_transaction
+    end
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,13 +5,19 @@ describe 'Transaction' do
 
   describe 'add_deposit' do
     it 'lets you add a deppsit' do
-      expect(transaction.add_deposit(1000)).to include('1000')
+      expect(transaction.add_deposit(1000.00)).to eq('04/23/2018 || 1000.00 ||   || 1000.00')
     end
   end
 
   describe 'withdraw' do
     it 'lets you withdraw money' do
-      expect(transaction.withdraw(400)).to include('600')
+      expect(transaction.withdraw(400.00)).to eq('04/23/2018 ||   || 400.00 || 600.00')
+    end
+  end
+
+  describe 'print' do
+    it 'shows all transaction history' do
+      expect(transaction.print).to eq( ["date || credit || debit || balance", "04/23/2018 ||   || 400.00 || 600.00", "04/23/2018 || 1000.00 ||   || 1000.00"])
     end
   end
 end
